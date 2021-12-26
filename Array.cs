92949f51fecda72fbd1cb7ce00a3123348831609
Vishal Mandal <94603068+vishalm965@users.noreply.github.com>
2021-12-26 16:06:08 +0530

    Add files via upload

---
+++ Array.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 10, 12, 100, 101, 110 };
+            int len = arr.Length;
+            Console.WriteLine("Length of Array" + len);
+            Console.WriteLine(arr[0] + "" + arr[4]);
+        }
+    }
+}
