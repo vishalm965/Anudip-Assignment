6565c6dd4f326c05a7da4a2ea192c8e6e56da073
Vishal Mandal <94603068+vishalm965@users.noreply.github.com>
2021-12-26 16:27:40 +0530

    Add files via upload

---
+++ GetHash.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gethash_Array
+{
+    using System;
+    using System.Text;
+    namespace object_Class_Gethashcode
+    {
+        class Program
+        {
+            class HashC
+            {
+                string name;
+                public HashC(string name)
+                {
+                    this.name = name;
+                }
+                public override int GetHashCode()
+                {
+                    string n = ToString();
+                    return n.GetHashCode();
+                }
+
+            }
+            static void Main(string[] args)
+            {
+                HashC ob1 = new HashC("vishal mandal");
+                Console.WriteLine(ob1.GetHashCode());
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Index.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+
+    using System;
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            string a = "CodesDope";
+            Console.WriteLine(a.IndexOf('D'));
+        }
+    }
+}
