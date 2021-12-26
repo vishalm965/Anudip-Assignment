9d8711ac601fd5d10284b37eccc02e50a9d4b336
Vishal Mandal <94603068+vishalm965@users.noreply.github.com>
2021-12-26 16:23:41 +0530

    Add files via upload

---
+++ equal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    using System;
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            string a = "Codes";
+            string b = "Dope";
+            Console.WriteLine(a.Equals(b));
+        }
+    }
+}
