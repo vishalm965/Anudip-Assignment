53c6dd1612ad8b8dd655098bc75d57c8c80aa429
Vishal Mandal <94603068+vishalm965@users.noreply.github.com>
2021-12-26 16:21:03 +0530

    Add files via upload

---
+++ Double dimensional.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, j;
+            int[,] arr = new int[2, 2];
+            Console.WriteLine("Enter the elements for 2X2 matrix");
+            for (i = 0; i < 2; i++)
+            {
+                for (j = 0; j < 2; j++)
+                {
+                    Console.Write("Enter element at [{0},{1}]=", i, j);
+                    arr[i, j] = Convert.ToInt32(Console.ReadLine());
+
+                }
+            }
+            for (i = 0; i < 2; i++)
+            {
+                Console.Write("\n");
+                for (j = 0; j < 2; j++)
+                {
+                    Console.Write("{0}\t", arr[i, j]);
+                }
+            }
+        }
+    }
+}
