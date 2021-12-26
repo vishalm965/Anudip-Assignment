86982b6d2149d1498157da5920b9c893e5888657
Vishal Mandal <94603068+vishalm965@users.noreply.github.com>
2021-12-26 16:16:47 +0530

    Add files via upload

---
+++ Div.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex05_Div_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(74 / 8);
+        }
+    }
+}
