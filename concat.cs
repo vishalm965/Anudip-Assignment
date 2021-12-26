6bd6822b17a18e69a83a8f221a2b3a1fccff2769
Vishal Mandal <94603068+vishalm965@users.noreply.github.com>
2021-12-26 16:16:25 +0530

    Add files via upload

---
+++ concat.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    using System;
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            string a = "Codes";
+            string b = "Dope";
+            String c = String.Concat(a, b);
+            Console.WriteLine(c);
+        }
+    }
+}
