48875c617e4b4059a06b499ad1b81cf6c9c1346c
Vishal Mandal <94603068+vishalm965@users.noreply.github.com>
2021-12-26 16:12:10 +0530

    Add files via upload

---
+++ Operator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace As_operator
+{
+
+    using System;
+    public class Demo
+    {
+        public static void Main()
+        {
+            object[] obj = new object[2];
+            obj[0] = "jack";
+            obj[1] = 32;
+            for (int i = 0; i < obj.Length; ++i)
+            {
+                string s = obj[i] as string;
+                Console.Write("{0}: ", i);
+                if (s != null)
+                    Console.WriteLine("'" + s + "'");
+                else
+                    Console.WriteLine("This is not a string!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
